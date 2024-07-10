514ba78b658608db87481d40c0492043b4d53d96
Rupesh Patil <rp61223456@gmail.com>
2024-07-10 11:16:13 +0530

    modified

--- src/services/request.tsx
+++ src/services/request.tsx
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'http://localhost:8000/api/v1/'; // Update with your backend URL
-const accessToken = localStorage.getItem('accessToken');
+const baseURL = "http://localhost:8000/api/v1/";
+const accessToken = localStorage.getItem("accessToken");
 export const axiosInstance = axios.create({
   baseURL,
   headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${accessToken}`
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
   },
 });
